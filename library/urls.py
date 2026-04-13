3dcd1d0e6e2ed91f6472bc99a1ad226fc3ffa865
ManuelMelgarejoJ <manuelmelgarejo2026@gmail.com>
2026-04-13 14:05:07 +0200

    url changes

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import (health,create_library_entry,list_library_entries,get_library_entry)
+from .views import (health,create_library_entry,list_library_entries,get_library_entry,delete_library_entry)
 
 urlpatterns = [
     path("health/", health),
     path("api/library/entries/", create_library_entry),
     path("api/library/entries/list/", list_library_entries),
     path("api/library/entries/<int:entry_id>/", get_library_entry),
+    path("api/library/entries/<int:entry_id>/delete/", delete_library_entry),
+
 ]
