de2b371f1ea6bb5e2fb6fc430e903ac566d3fc42
ManuelMelgarejoJ <manuelmelgarejo2026@gmail.com>
2026-04-16 09:14:10 +0200

    feat: add update_library_entry endpoint to urls.py

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import (health,create_library_entry,list_library_entries,get_library_entry,delete_library_entry)
+from .views import (
+    health,
+    create_library_entry,
+    list_library_entries,
+    get_library_entry,
+    update_library_entry,
+    delete_library_entry
+)
 
 urlpatterns = [
     path("health/", health),
@@ -7,5 +14,5 @@ urlpatterns = [
     path("api/library/entries/list/", list_library_entries),
     path("api/library/entries/<int:entry_id>/", get_library_entry),
     path("api/library/entries/<int:entry_id>/delete/", delete_library_entry),
-
+    path("api/library/entries/<int:entry_id>/update/", update_library_entry),
 ]
